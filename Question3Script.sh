471deae2fdcd6e76b6478a038280cdfe5fb1c06d
Anirban_Nandy <anirbanironman50@gmail.com>
2024-11-29 15:58:00 +0530

    Modified Question3

--- Question3Script.sh
+++ Question3Script.sh
@@ -2,12 +2,23 @@
 
 myfile="Question3.log"
 
+declare -A ticket_count
+
 while IFS=" " read -r line
 do
 	time=$(echo "$line" | awk '{print $1}')
 	#echo "$time"
 	user=$(echo "$line" | awk '{print $3}')
-	echo "$user"
+	#echo "$user"
 	booking=$(echo "$line" | awk '{print $4}')
-	echo "$booking"
+	#echo "$booking"
+	
+	if [ "$booking" == "booked" ]; then
+		((ticket_count["$user"]++))
+	elif [ "$booking" == "cancelled" ]; then
+		((ticket_count["$user"]--))
+	fi
+
+	echo " User: $user      |  Count: ${ticket_count["$user"]}"
+	
 done < "$myfile"
